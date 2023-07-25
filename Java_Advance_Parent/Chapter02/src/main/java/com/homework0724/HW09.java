2b05ee05da1242582f8d5bd7dc2cd309b9e434ed
Freesky030107 <506129326@qq.com>
2023-07-25 11:20:23 +0800

    homework0724

---
+++ Java_Advance_Parent/Chapter02/src/main/java/com/homework0724/HW09.java
@@ -0,0 +1,21 @@
+package com.homework0724;
+
+import java.util.Scanner;
+
+public class HW09 {
+    public static void main(String[] args) {
+        String username = "TOM";
+        String password = "1234567";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入用户名：");
+        String inputUsername = input.next();
+        System.out.print("请输入密码：");
+        String inputPassword = input.next();
+        if (username.equalsIgnoreCase(inputUsername) && password.equals(inputPassword)) {
+            System.out.println("登录成功！");
+        } else {
+            System.err.println("用户名或密码不匹配，登录失败！");
+        }
+    }
+}
