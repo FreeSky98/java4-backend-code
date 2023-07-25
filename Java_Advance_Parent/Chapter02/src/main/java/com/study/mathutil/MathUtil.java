a8a0f0882528a5346c217f7b43523af6b96c62be
Freesky030107 <506129326@qq.com>
2023-07-25 14:55:38 +0800

    test

---
+++ Java_Advance_Parent/Chapter02/src/main/java/com/study/mathutil/MathUtil.java
@@ -0,0 +1,12 @@
+package com.study.mathutil;
+import static java.lang.Math.*;
+public class MathUtil {
+    public static void main(String[] args) {
+        System.out.println("abs(4) = " + abs(4));
+        System.out.println("abs(0) = " + abs(0));
+        System.out.println("abs(-4) = " + abs(-4));
+
+        System.out.println("abs(Integer.MAX_VALUE) = " + abs(Integer.MAX_VALUE));
+        System.out.println("abs(Integer.MIN_VALUE) = " + abs(Integer.MIN_VALUE));
+    }
+}
